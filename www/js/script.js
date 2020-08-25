ebee2219846dcf955403bc08d7d5e803da109e1f
hnramba <47969375+hnramba@users.noreply.github.com>
2020-08-25 11:27:32 +0400

    Add files via upload

--- www/js/script.js
+++ www/js/script.js
@@ -1,23 +1,31 @@
-$(document).ready(function () {
-  var latitude = 0;
-  var longitude = 0;
-  var map, map2, map3;
+var latitude = 0;
+var longitude = 0;
+var map, map2, map3;
 
+$(document).ready(function () {
   navigator.geolocation.getCurrentPosition(userLocated, locationError);
 
   $(document).on("pagechange", function () {
     map.invalidateSize();
   });
 
-  $("#btnSearch").click(function () {
-    map2 = L.map("map2").setView([-20.244959, 57.561768], 10);
+  function userLocated(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    map = L.map("map").setView([latitude, longitude], 15);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map2);
+    }).addTo(map);
 
-    var searchControl = L.esri.Geocoding.geosearch().addTo(map2);
+    L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup("This is My Location")
+      .openPopup();
+    map.invalidateSize();
+
+    var searchControl = L.esri.Geocoding.geosearch().addTo(map);
     searchControl.on("results", function (data) {
       results.clearLayers();
       for (var i = data.results.length - 1; i >= 0; i--) {
@@ -25,12 +33,9 @@ $(document).ready(function () {
       }
     });
 
-    $(document).on("pagechange", function () {
-      map2.invalidateSize();
-    });
     var geocodeService = L.esri.Geocoding.geocodeService();
 
-    map2.on("click", function (e) {
+    map.on("click", function (e) {
       geocodeService
         .reverse()
         .latlng(e.latlng)
@@ -40,35 +45,11 @@ $(document).ready(function () {
           }
 
           L.marker(result.latlng)
-            .addTo(map2)
+            .addTo(map)
             .bindPopup(result.address.Match_addr)
             .openPopup();
         });
     });
-  });
-
-  function userLocated(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    map = L.map("map").setView([latitude, longitude], 15);
-
-    L.tileLayer(
-      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 19,
-        id: "mapbox/streets-v11",
-        accessToken:
-          "pk.eyJ1Ijoic2hpdmFuZWUxNyIsImEiOiJja2U4OXg3aDMxdW43MzFuN2ptd290czBzIn0.hAR7nJ4z4pp2iIUOAI9r1g",
-      }
-    ).addTo(map);
-
-    L.marker([latitude, longitude])
-      .addTo(map)
-      .bindPopup("This is My Location")
-      .openPopup();
-    map.invalidateSize();
   }
   function locationError(error) {
     alert("Error code: " + error.code);
@@ -85,3 +66,40 @@ $(document).ready(function () {
     }
   }
 });
+function searchloc() {
+  map2 = L.map("map2").setView([-20.244959, 57.561768], 10);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map2);
+
+  var searchControl = L.esri.Geocoding.geosearch().addTo(map2);
+  searchControl.on("results", function (data) {
+    results.clearLayers();
+    for (var i = data.results.length - 1; i >= 0; i--) {
+      results.addLayer(L.marker(data.results[i].latlng));
+    }
+  });
+
+  $(document).on("pagechange", function () {
+    map2.invalidateSize();
+  });
+  var geocodeService = L.esri.Geocoding.geocodeService();
+
+  map2.on("click", function (e) {
+    geocodeService
+      .reverse()
+      .latlng(e.latlng)
+      .run(function (error, result) {
+        if (error) {
+          return;
+        }
+
+        L.marker(result.latlng)
+          .addTo(map2)
+          .bindPopup(result.address.Match_addr)
+          .openPopup();
+      });
+  });
+}
